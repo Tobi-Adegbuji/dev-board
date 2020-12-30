070446a69d6ad1c8e8baccba721e4438f7311ab1
Tobi <adegbuji_tobi@yahoo.com>
2020-12-30 16:08:05 -0500

    Adjusted sidebar css

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,7 +6,10 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
+        <img
+          src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          alt=""
+        />
         <Avatar className="sidebar__avatar" />
         <h2>Tobi Adegbuji</h2>
         <h4>A dedicated programmer</h4>
